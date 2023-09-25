9019fa1df21b4d13f341b5d0112a652ae708200d
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-09-25 17:43:27 +0200

    Added github link

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        <a href="https://github.com/Radwa-Hafez/trial-weather-react">
+          Open-source code
+        </a>
+        <span> by Radwa Hafez 💜</span>
+      </p>
+    </div>
+  );
+}
